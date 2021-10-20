6da09a933403613d586c945c07eb886d062c372a
arseniy <arseniy.karmanov@mail.ru>
2021-10-20 20:07:07 +0700

    LAB#5 done

---
+++ lab5_55.py
@@ -0,0 +1,12 @@
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z']
+step = 0
+old_value = 0
+print(abc)
+while step < 25:
+    for i in range(0, len(abc) - 1):
+        old_value = abc[i + 1]
+        abc[i + 1] = abc[i]
+        abc[i] = old_value
+    print(abc)
+    step += 1
